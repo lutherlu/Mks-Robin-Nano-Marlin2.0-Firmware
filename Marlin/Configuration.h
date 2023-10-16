ba90e744616e7cfd3bb9c73f8ea0ed2c47292dce
lujiangwen <lujiangwen@gmail.com>
2023-10-16 09:07:42 +0800

    LOCAL Changes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1,4 +1,4 @@
-/**
+/**=====================================================
  * Marlin 3D Printer Firmware
  * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
  *
@@ -143,7 +143,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-// #define CUSTOM_MACHINE_NAME "3D Printer"
+#define CUSTOM_MACHINE_NAME "BigTree"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
